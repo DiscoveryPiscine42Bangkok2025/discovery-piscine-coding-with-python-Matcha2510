3bca03d874006df3f6572b3f599bbf9eaabaa3f1
Matcha2510 <65050411@kmitl.ac.th>
2025-09-17 20:18:15 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+#cell 2-1
+num1 =  int(input())      
+if num1 > 0 :
+    print("This number is positive") 
+elif num1 < 0 :
+    print("This number is negative") 
+else : 
+    print("This number is both positive and negaitive")
