89167c4803220112ac524d8c71e8c595f9ab2409
Matcha2510 <65050411@kmitl.ac.th>
2025-09-17 13:55:11 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,10 @@
+#cell 2-3
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the secound number: "))
+result = num1 * num2
+if result > 0 :
+    print("The result is positive")
+elif result < 0 :
+    print("The result is negative")
+else :
+    print("The result is positive and negative")
